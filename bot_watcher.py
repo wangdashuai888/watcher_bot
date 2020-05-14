17900aa04b33e57b01cf119bf4e07eacf47589fc
BaldPulse <35502017+BaldPulse@users.noreply.github.com>
2020-05-14 00:54:40 -0700

    Update bot_watcher.py

--- bot_watcher.py
+++ bot_watcher.py
@@ -1,6 +1,7 @@
 import discord
 import queue
 import asyncio
+import datetime
 
 from discord.ext import commands
 from discord.ext import tasks
@@ -43,23 +44,23 @@ async def on_message(msg):
     #print("inside",misc_channel,type(misc_channel))
     if msg.channel.id != misc_channel and msg.content.startswith("!"):
         q.put(msg) #queue for print message
-        print('put', msg.id)
+        print(datetime.datetime.now(), 'put', msg.id)
         await msg.delete()        
     if msg.channel.id != misc_channel and msg.author.id == misc_bot:
         q.put(msg) #queue for print message
-        print('put', msg.id)
+        print(datetime.datetime.now(), 'put', msg.id)
         await msg.delete()
     await bot.process_commands(msg)
 
-@tasks.loop(seconds = 1)
+@tasks.loop(seconds = 2)
 async def watcher():
-    print('task')
     global q
     ch = bot.get_channel(misc_channel)
+    if not q.empty():
+        print(datetime.datetime.now(), ': awakened')
     while not q.empty():
-        print('in loop', q.queue[0].id)
         msg = q.get()
-        print('processing id', msg.id)
+        print('\t', datetime.datetime.now(), 'processing id', msg.id)
         if msg.author.id != misc_bot:
             await ch.send(msg.author.mention + " ***All music commands needs to be send in this channel***")
             await ch.send(msg.content)
@@ -72,4 +73,4 @@ async def watcher():
         
 watcher.start()
 
-bot.run('Your Token')
+bot.run('yourToken')
