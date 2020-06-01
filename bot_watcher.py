b2f9a2b0c6279df622133c37c8095986d1fd7cf5
wangdashuai888 <56279107+wangdashuai888@users.noreply.github.com>
2020-05-31 22:22:24 -0400

    updated multi-server support
    v0.1.1
    updated multi-server support

--- bot_watcher.py
+++ bot_watcher.py
@@ -2,66 +2,83 @@ import discord
 import queue
 import asyncio
 import datetime
+import os
 
+from dotenv import load_dotenv
 from discord.ext import commands
 from discord.ext import tasks
 
-#client = discord.Client()
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
 bot = commands.Bot(command_prefix='$')
-
 q = queue.Queue()
 
 channel_set = False
-#misc_channel = 0
-misc_channel = 564671943106887695
-#misc_bot = 0
-misc_bot = 235088799074484224
+misc_channel = {}
+#misc_channel = 630354263134044177
+misc_bot = {}
+#misc_bot = 235088799074484224
 
 @bot.event
 async def on_ready():
     print(f'{bot.user} has connected to Discord!')
 
-@bot.command(name = 'zhao')
-async def _zhao(ctx, arg: int):
+@bot.command(name = 'channel')
+async def _channel(ctx, arg: int):
     global misc_channel
-    misc_channel = arg
+    guild_id = ctx.guild.id
+    misc_channel[guild_id] = arg
     channel_set = True
-    print("Music Channel set to ： ", misc_channel)
+    print("catch a channel command in server: ", guild_id , arg)
+    print("Full list: ", misc_channel)
     await ctx.send("Music channel set up complete, channel id: " + str(arg))
 
-
-@bot.command(name = 'lis')
-async def _lis(ctx, arg: int):
+@bot.command(name = 'player')
+async def _player(ctx, arg: int):
     global misc_bot
-    misc_bot = arg
-    print("Music bot set to: ", misc_bot)
+    guild_id = ctx.guild.id
+    misc_bot[guild_id] = arg
+    print("catch a player command in server: ", guild_id , arg)
+    print("Full list: ", misc_bot)
     await ctx.send("music bot set as: " + str(arg))
+
+@bot.command(name = 'blk_list')
+async def _blk(ctx, arg):
+    player = arg
+    print("arg")
+
 
 @bot.event
 async def on_message(msg):
     global q
+    guild_id = msg.guild.id
+    print(guild_id)
     #print('processing message id', msg.id)
     #print("inside",misc_channel,type(misc_channel))
-    if msg.channel.id != misc_channel and msg.content.startswith("!"):
-        q.put(msg) #queue for print message
-        print(datetime.datetime.now(), 'put', msg.id)
-        await msg.delete()        
-    if msg.channel.id != misc_channel and msg.author.id == misc_bot:
-        q.put(msg) #queue for print message
-        print(datetime.datetime.now(), 'put', msg.id)
-        await msg.delete()
-    await bot.process_commands(msg)
+    if msg.content.startswith("$"):
+        await bot.process_commands(msg)
+    else:
+        if msg.channel.id != misc_channel[guild_id] and msg.content.startswith("!"):
+            q.put(msg) #queue for print message
+            print(datetime.datetime.now(), 'put', msg.id)
+            await msg.delete()
+        if msg.channel.id != misc_channel[guild_id] and msg.author.id == misc_bot[guild_id]:
+            q.put(msg) #queue for print message
+            print(datetime.datetime.now(), 'put', msg.id)
+            await msg.delete()
+        await bot.process_commands(msg)
 
 @tasks.loop(seconds = 2)
 async def watcher():
     global q
-    ch = bot.get_channel(misc_channel)
     if not q.empty():
         print(datetime.datetime.now(), ': awakened')
     while not q.empty():
         msg = q.get()
+        guild_id = msg.guild.id
+        ch = bot.get_channel(misc_channel[guild_id])
         print('\t', datetime.datetime.now(), 'processing id', msg.id)
-        if msg.author.id != misc_bot:
+        if msg.author.id != misc_bot[guild_id]:
             await ch.send(msg.author.mention + " ***All music commands needs to be send in this channel***")
             await ch.send(msg.content)
         elif msg.embeds != []:
@@ -70,7 +87,7 @@ async def watcher():
             await ch.send(msg.content)
     #await bot.wait_until_ready()
     #counter = 0
-        
+
 watcher.start()
 
-bot.run('yourToken')
+bot.run(TOKEN)
